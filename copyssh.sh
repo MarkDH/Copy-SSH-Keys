acc389e842bf29f5fa5be0ec68818f39378f1de1
MarkDH <MarkDH@users.noreply.github.com>
2016-05-20 14:04:14 +0200

    Update copyssh.sh

--- copyssh.sh
+++ copyssh.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
+echo "Thank you for using this SSH-Key copy script"
+echo ""
+echo "Press ENTER to continue"
+echo ""
+read input
+echo ""
+echo "Do you want to create new SSH-Keys? [Y/N]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
 
-ssh-keygen -t rsa
+        ssh-keygen -t rsa
 
-echo "Receiver IP Address:"
-read ip
-if [[ "$ip" =~ ^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$ ]]
-then
-    for (( i=1; i<${#BASH_REMATCH[@]}; ++i ))
-    do
-      (( ${BASH_REMATCH[$i]} <= 255 )) || { echo "BAD IP!" >&2; exit 1; }
-    done
+
 else
-      echo "BAD IP!" >&2
-      exit 1;
+
+        echo "Receiver IP Address:"
+        read ip
+        if [[ "$ip" =~ ^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$ ]]
+        then
+            for (( i=1; i<${#BASH_REMATCH[@]}; ++i ))
+            do
+              (( ${BASH_REMATCH[$i]} <= 255 )) || { echo "BAD IP!" >&2; exit 1; }
+            done
+        else
+              echo "BAD IP!" >&2
+              exit 1;
+        fi
+
+        echo "Receiver Username"
+        read name
+        name=${name// /_}
+
+        sudo ssh-copy-id $name@$ip
+
+        echo
+        echo
+        echo
+        echo
+        echo "---"
+        echo "SSH KEY HAS BEEN COPIED TO $name at $ip"
+        echo "---"
+        echo
+        echo
+        echo
+        echo
+
 fi
-
-echo "Receiver Username"
-read name
-name=${name// /_}
-
-sudo ssh-copy-id $name@$ip
-
-echo
-echo
-echo
-echo
-echo "---"
-echo "SSH KEY HAS BEEN COPIED TO $name at $ip"
-echo "---"
-echo
-echo
-echo
-echo
