c1ed325a0022b4184a027dcc323b8a5fd20b71db
MarkDH <MarkDH@users.noreply.github.com>
2016-05-20 15:48:17 +0200

    Update copyssh.sh

--- copyssh.sh
+++ copyssh.sh
@@ -1,68 +1,27 @@
 #!/bin/bash
+
+topsign='\E[34;34m'"\033[1m\n\n  _____ ____  _______     __   _____  _____ _    _\n / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | |\n| |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| |\n| |   | |  | |  ___/ \   /    \___ \ \___ \|  __  |\n| |___| |__| | |      | |     ____) | ___) | |  | |\n \_____\____/|_|      |_|    |_____/ \____/|_|  |_|\n\n                            By - Mark Dinsen-Hansen \n \n \n \n\033[0m" && tput sgr0
+blue="$(tput setaf 4)"
+default="$(tput sgr0)"
+
 clear
-echo ""
-echo "   _____ ____  _______     __   _____  _____ _    _  "
-echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-echo "                                                     "
-echo "                              By - Mark Dinsen-Hansen"
-echo ""
-echo ""
-echo ""
-echo "Thank you for using this SSH-Key copy script"
-echo ""
-echo ""
+echo -e "$topsign"
+#echo "Thank you for using this SSH-Key copy script"
 echo -n "Press ENTER to continue"
 read input
 clear
-echo ""
-echo "   _____ ____  _______     __   _____  _____ _    _  "
-echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-echo "                                                     "
-echo "                              By - Mark Dinsen-Hansen"
-echo ""
-echo ""
-echo ""
+echo -e "$topsign"
 echo -n "Do you want to create new SSH-Keys? [Y/N]: "
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
         clear
-        echo ""
-        echo "   _____ ____  _______     __   _____  _____ _    _  "
-        echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-        echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-        echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-        echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-        echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-        echo "                                                     "
-        echo "                              By - Mark Dinsen-Hansen"
-        echo ""
-        echo ""
-        echo ""
+        echo -e "$topsign"
         ssh-keygen -t rsa
 
 
 else
         clear
-        echo ""
-        echo "   _____ ____  _______     __   _____  _____ _    _  "
-        echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-        echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-        echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-        echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-        echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-        echo "                                                     "
-        echo "                              By - Mark Dinsen-Hansen"
-        echo ""
-        echo ""
-        echo ""
+        echo -e "$topsign"
         echo -n "Receiver IP Address: "
         read ip
         if [[ "$ip" =~ ^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$ ]]
@@ -76,38 +35,16 @@ else
               exit 1;
         fi
         clear
-        echo ""
-        echo "   _____ ____  _______     __   _____  _____ _    _  "
-        echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-        echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-        echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-        echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-        echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-        echo "                                                     "
-        echo "                              By - Mark Dinsen-Hansen"
-        echo ""
-        echo ""
-        echo ""
-        echo -n "Receiver Username: "
+        echo -e "$topsign"
+        echo -n "Reveiver Username: "
         read name
-        name=${name// /_}
-
-        sudo ssh-copy-id $name@$ip
-
         clear
-        echo ""
-        echo "   _____ ____  _______     __   _____  _____ _    _  "
-        echo "  / ____/ __ \|  __ \ \   / /  / ____ / ____| |  | | "
-        echo " | |   | |  | | |__) \ \_/ /  | (___ | (___ | |__| | "
-        echo " | |   | |  | |  ___/ \   /    \___ \ \___ \|  __  | "
-        echo " | |___| |__| | |      | |     ____) | ___) | |  | | "
-        echo "  \_____\____/|_|      |_|    |_____/ \____/|_|  |_| "
-        echo "                                                     "
-        echo "                              By - Mark Dinsen-Hansen"
-        echo ""
-        echo ""
-        echo ""
+        name=${name// /_}
+        sudo ssh-copy-id $name@$ip
+        clear
+        echo -e "$topsign"
         echo "SSH KEY HAS BEEN COPIED TO $name at $ip!"
+        echo -e "\n\n"
         echo -n "Press ENTER to exit! "
         read input
         clear
